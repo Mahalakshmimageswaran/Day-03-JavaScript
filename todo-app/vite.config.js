32d0327407e8ee0749d5692c999e3e70b5f5097d
Mahalakshmi Mageswaran <mahalakshrimageswaran@gmail.com>
2025-12-04 18:59:10 +0530

    second paages

--- todo-app/vite.config.js
+++ todo-app/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Day-03-JavaScript/todo-app/',   // 👈 very important for GitHub Pages
+  base: '/Day-03-JavaScript/todo-app/',
   plugins: [react()],
 })
