88b2d824d846cca8d322f7eb34eb461d439424fa
Mahalakshmi Mageswaran <mahalakshrimageswaran@gmail.com>
2025-12-04 18:55:13 +0530

    pages

--- todo-app/vite.config.js
+++ todo-app/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Day-03-JavaScript/todo-app/',   // 👈 very important for GitHub Pages
   plugins: [react()],
-  base: "/todo-app/",   // folder name only
-});
+})
